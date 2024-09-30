e3f686f472960513ebe03169873e5b7869054fd4
mdbromhal <mdalia@proton.me>
2024-09-30 14:46:02 -0400

    created basic sign-up page

--- AIPlanner/AIPlanner/AIPlanner.py
+++ AIPlanner/AIPlanner/AIPlanner.py
@@ -1,8 +1,8 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-
 from rxconfig import config
+# from pages.signup import signup # Sign up page
 
 #to run test environment
 #>cd AIPlanner
@@ -33,6 +33,11 @@ def index() -> rx.Component:
                 href="https://reflex.dev/docs/getting-started/introduction/",
                 is_external=True,
             ),
+            rx.link(
+                rx.button("Sign Up!"),
+                href="/signup",
+                is_external=False,
+            ),
             spacing="5",
             justify="center",
             min_height="85vh",
@@ -40,6 +45,29 @@ def index() -> rx.Component:
         rx.logo(),
     )
 
+def signup() -> rx.Component:
+    # Signup page 
+    return rx.container(
+        rx.vstack(
+            rx.heading("Sign Up!", size="0"),
+            rx.input(placeholder="Enter email address", size='lg'),
+            rx.input(placeholder="Enter password", type="password", size='lg'),
+            #rx.button("Submit", on_click=lambda: print("Signed up!")),
+            spacing="5",
+            justify="center",
+            min_height="85vh"
+        ),
+        rx.logo()
+    )
 
 app = rx.App()
 app.add_page(index)
+
+
+# Megdalia Bromhal - 30 Sept. 2024
+# Adding a signup page (as defined in pages.signup)
+app.add_page(signup)
+
+if __name__ == "__main__":
+    app.run()
+# Eof
