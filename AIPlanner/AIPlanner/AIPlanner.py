12606a62ba6d4a26d7c98dd1231264d324c2fa5b
Archer James <kajames234@gmail.com>
2024-10-13 19:07:10 -0400

    I pulled from main and messed it up I have to recommit it

--- AIPlanner/AIPlanner/AIPlanner.py
+++ AIPlanner/AIPlanner/AIPlanner.py
@@ -1,13 +1,17 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-from rxconfig import config
-# from pages.signup import signup # Sign up page
 
-#to run test environment
-#>cd AIPlanner
-#>py -3 -m venv .venv
-#>reflex run
+
+# from pages.signup import signup  # Sign up page
+
+from AIPlanner.CreateCal import GenCalendar
+
+
+# to run test environment
+# >cd AIPlanner
+# >py -3 -m venv .venv
+# >reflex run
 # open http://localhost:3000/
 
 
@@ -16,37 +20,63 @@ class State(rx.State):
 
     ...
 
-
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Clicks!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            rx.link(
-                rx.button("Sign Up!"),
-                href="/signup",
-                is_external=False,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
+    # Home Page - Calendar
+    return rx.center(
+        calendar_component(),
+        padding="50px",
+        
     )
 
+month_year = GenCalendar.get_month_year_label
+
+def calendar_component():
+    return rx.vstack(
+        # Display current month and year
+        rx.button("Load Calendar", on_click=GenCalendar.init_calendar),
+        # Navigation buttons for previous and next months
+        
+        rx.hstack(
+            rx.button("Previous", on_click=GenCalendar.prev_month),
+            rx.button("Next", on_click=GenCalendar.next_month),
+        ),
+
+        # Create the table for the calendar
+        rx.heading(GenCalendar.label, size = "lg"),
+                # Table header for days of the week
+        rx.table.root(
+            
+            rx.table.header(
+                rx.table.row(
+                    rx.table.column_header_cell("Sun", scope = "col"),
+                    rx.table.column_header_cell("Mon",scope = "col"),
+                    rx.table.column_header_cell("Tues",scope = "col"),
+                    rx.table.column_header_cell("Wed",scope = "col"),
+                    rx.table.column_header_cell("Thurs",scope="col"),
+                    rx.table.column_header_cell("Fri",scope = "col"),
+                    rx.table.column_header_cell("Sat",scope = "col"),
+                    )
+                ),
+            
+                # Table body for days in the month
+            rx.table.body(
+                rx.foreach(GenCalendar.dates, lambda week: rx.table.row(
+                    rx.foreach(week, lambda day: rx.table.cell(day, text_align="center", padding="10px"))
+    ))),
+        ),
+        width="100%",
+    
+            padding="20px",
+        ),
+        
+    
+
+
+
+
+
 def signup() -> rx.Component:
-    # Signup page 
+    # Signup page
     return rx.container(
         rx.vstack(
             rx.heading("Sign Up!", size="0"),
@@ -55,7 +85,7 @@ def signup() -> rx.Component:
             rx.button("Submit"),  # on_click=lambda: print("Signed up!")
             rx.link(
                 rx.button("Go back"),
-                href = "/",
+                href="/",
                 is_external=False,
             ),
             spacing="5",
@@ -65,9 +95,9 @@ def signup() -> rx.Component:
         rx.logo()
     )
 
+
 app = rx.App()
 app.add_page(index)
-
 
 # Megdalia Bromhal - 30 Sept. 2024
 # Adding a signup page (as defined in pages.signup)
---
+++ AIPlanner/AIPlanner/CreateCal.py
@@ -0,0 +1,80 @@
+import calendar
+from datetime import datetime
+import reflex as rx
+
+
+class GenCalendar(rx.State):
+    """
+    Generate Calendar class
+
+    now : datetime 
+        current date 
+    current_month :
+        current month to start at
+    current_year:
+        current year to start at
+    dates: list
+        list of weeks of dates of the year
+    label: string
+        Title of the calendar
+    """
+    now = datetime.now()
+    current_month: int = now.month
+    current_year: int = now.year
+    dates: list[list[str]] = []
+    label = ""
+
+
+    def days_in_month(self):
+        """
+         Return first weekday of the month, and number of days in the month
+        """
+        cal = calendar.Calendar(firstweekday=6)  # Start the week on Sunday
+        return calendar.monthrange(self.current_year, self.current_month)
+
+    def get_month_year_label(self):
+        """Set the label variable to a string of the month and year for Title of calendar"""
+        month_name = calendar.month_name[self.current_month]
+        month_year_string = str(month_name + " "+str(self.current_year))
+        self.label =  month_year_string
+
+    def next_month(self):
+        """increment month and reinitialize calendar"""
+        if self.current_month == 12:
+            self.current_month = 1
+            self.current_year += 1
+        else:
+            self.current_month += 1
+        self.init_calendar()
+
+    def prev_month(self):
+        """Decrement month and reinitialize calendar"""
+        if self.current_month == 1:
+            self.current_month = 12
+            self.current_year -= 1
+        else:
+            self.current_month -= 1
+        self.init_calendar()
+   
+    def make_dates(self):
+        """
+        Set dates list as list of numbers of days to iterate through
+        """
+        self.dates = []
+        first_day, days = self.days_in_month()
+        days_in_month = [datetime(self.current_year, self.current_month, day) 
+                             for day in range(1, days + 1)]
+        week = [0] * first_day
+        for day in days_in_month:
+            week.append(day.day)
+            if len(week) == 7:
+                self.dates.append(week)
+                week = []
+        if week:
+            week.extend([0]*(7-len(week)))
+            self.dates.append(week)
+
+    def init_calendar(self):
+        """Runs the initialization of variables"""
+        self.make_dates()
+        self.get_month_year_label()
