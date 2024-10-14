9d1a980030f07f885d9893837502dda008da083f
rla1743 <rla1743@uncw.edu>
2024-10-13 22:47:17 -0400

    Added docstrings to several functions and classes in database.py, adjustments made to satisfy Pylint requirements

--- AIPlanner/AIPlanner/classes/database.py
+++ AIPlanner/AIPlanner/classes/database.py
@@ -1,16 +1,27 @@
+"""Module containing classes and methods pertaining to the SQLite database built into Reflex"""
+from datetime import date, time, timedelta
 import reflex as rx
 from rxconfig import config
-from datetime import date, time, timedelta
 
 class User(rx.Model, table=True):
+    """Class that defines the User table in the SQLite database
+    
+    Attributes:
+        username: A unique string identifier, allows users 
+                  to sign in without a canvas_hash_id specified
+        canvas_hash_id: A unique integer identifier, allows tasks to be imported from Canvas
+        password: A string used to authenticate user login
+    """
     username: str
     canvas_hash_id: int
     password: str
 
     class Meta:
+        """Metadata for User class"""
         primary_key = "canvas_hash_id"
 
 class Task(rx.Model, table=True):
+    """Class that defines the Task table in the SQLite database"""
     recur_frequency: int
     due_date: date
     is_deleted: bool
@@ -23,35 +34,32 @@ class Task(rx.Model, table=True):
     assigned_block_duration: timedelta
 
 class UserManagementState(rx.State):
+    """Class that defines the state in which variables are held relating to user management"""
     users: list[User] = []  # To hold the list of users
     message: str = ""        # To display success or error messages
 
     def fetch_all_users(self):
+        """Method to retrieve all usernames in the database"""
         with rx.session() as session:
-                # Retrieve all users from the database
-                self.users = session.exec(User.select()).all()
-                self.message = f"Retrieved {len(self.users)} users."
-                print(self.users)
-        
+            # Retrieve all users from the database
+            self.users = session.exec(User.select()).all()
+            self.message = f"Retrieved {len(self.users)} users."
+            print(self.users)
+
 class AddUser(rx.State):
+    """Class that enables adding users to the database"""
     username: str
     canvas_hash_id: int
     password: str
 
-    def set_username(self, value: str):
-        self.username = value
-
-    def set_canvas_hash_id(self, value: int):
-        self.canvas_hash_id = value
-
-    def set_password(self, value: str):
-        self.password = value
-
     def add_user(self):
+        """Method to add a user to the database by starting and commiting an rx.session()"""
         with rx.session() as session:
             session.add(
                 User(
-                    username=self.username, canvas_hash_id=self.canvas_hash_id, password=self.password
+                    username=self.username,
+                    canvas_hash_id=self.canvas_hash_id,
+                    password=self.password
                 )
             )
             session.commit()
