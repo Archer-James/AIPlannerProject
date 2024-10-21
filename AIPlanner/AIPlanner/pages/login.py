d1cacd82a400bf0ca3da81e61d737593fbf39dca
mdbromhal <mdalia@proton.me>
2024-10-21 14:07:52 -0400

    cleaning up log in page

--- AIPlanner/AIPlanner/pages/login.py
+++ AIPlanner/AIPlanner/pages/login.py
@@ -13,20 +13,6 @@ import AIPlanner.classes.database as database
 # import AIPlanner.pages.processing as processing **Discuss in meeting**
 
 
-def check_passwords(password, password_check):
-    """
-    Checks if password is the same as the password check.
-
-    param password: rx password         the password the user enters
-    param password_check: rx password   the password the user enters into the check
-    returns True if passwords match, False otherwise
-    """
-    if password == password_check:
-        return True
-    else:
-        return False
-
-
 class LoginState(rx.State):
     """
     Login state.
@@ -46,12 +32,6 @@ class LoginState(rx.State):
         # Getting data from log in form
         self.email = login_data.get('email')
         self.password = login_data.get('password')
-
-        # Iterating through Reflex database
-        # for user in database.UserManagementState.users:
-        #     if (user.username == self.email) and (user.password == self.password):
-        #         user_found = True
-        #         break
         
         # Starting rx database session
         with rx.session() as session:
@@ -62,23 +42,6 @@ class LoginState(rx.State):
                 ),
             ).first()
 
-
-        # rx.foreach(database.UserManagementState.users, lambda user: (
-        #     user_found := (user.username == self.email) and (user.password == self.password))
-        # )
-
-    #     class QueryUser(rx.State):
-    # name: str
-    # users: list[User]
-
-    # def get_users(self):
-    #     with rx.session() as session:
-    #         self.users = session.exec(
-    #             User.select().where(
-    #                 User.username.contains(self.name)
-    #             )
-    #         ).all()
-
         # If user found, allow log in
         if user_found:
             print(f"User found: {user_found.username}, {user_found.password}, {user_found.canvas_hash_id}")
@@ -88,81 +51,6 @@ class LoginState(rx.State):
         # User not found
         else:
             return rx.toast("User not found with this email and password combination.")
-
-
-# def display_usernames(state=UserManagementState):
-#         """Function to display usernames"""
-#         return rx.vstack(
-#         rx.text(state.message),  # Display number of users retrieved
-#         rx.foreach(  # Use rx.foreach for list rendering
-#             state.users, 
-#             lambda user: rx.text(user.username)  # Create a text component for each username
-#         )
-#     )
-
-
-
-    def submit(self, signup_data):
-        """
-        Function that handles user's data when user signs up. Saves username and password into database.
-
-        param signup_data: data the user enters into the signup form (i.e. username and password)
-        """
-        self.email = signup_data.get('email')
-        self.password = signup_data.get('password')
-        self.password_check = signup_data.get('password_check')
-        
-        # Checking if password matches password check
-        if check_passwords(self.password, self.password_check) == True:
-            
-            # Checking that email is only 25 chars long max
-            if len(self.email) <= 25:
-                # Passwords match, so process account
-                print("Processing new account.")
-                #self.is_processing = True
-                #self.change_processing_msg()
-                # time.sleep(4)
-                # Need processing message!!!
-
-                # Adding account to database
-                try:
-                    # Create a new user with Reflex database
-                    database.create_user(username=self.email, canvas_hash_id=1, password=self.password)
-                
-                    #self.is_processing = False # Changing back just in case
-                    #self.change_processing_msg()
-
-                    print("Account created in rx database")
-                    return rx.redirect('/success')
-
-                # If error, tell user to try again
-                except ModuleNotFoundError as e:
-                    print(e)
-                    return rx.toast("Error occurred while saving data. Please try again.")
-
-            # Email is too long; tell user
-            else:
-                return rx.toast("Email is too long. Please enter appropriate email.")
-        
-        # If passwords don't match, ask user to re-enter data
-        else:
-            return rx.toast("Passwords do not match. Please enter information again.")
-    
-#         # Create a new user with csv testing database
-#         new_user, success_code = user.create_user(username=self.email, 
-#                                                   canvas_hash_id=None, 
-#                                                   password=self.password
-#                                                   )
-            
-
-    # @rx.var(cache=True) # Cached variable
-    # def set_processing_msg(self) -> str:
-    #     return f"{self.processing_msg}"
-
-
-    # @rx.var(cache=True)
-    # def change_processing_msg(self) ->str:
-    #     return str(self.processing_msg)
 
 
 def login_form() -> rx.Component:
@@ -211,6 +99,7 @@ def login_form() -> rx.Component:
         spacing="50",
         justify="center",
     )
+
 
 def login() -> rx.Component:
     """Base page for log in component"""
