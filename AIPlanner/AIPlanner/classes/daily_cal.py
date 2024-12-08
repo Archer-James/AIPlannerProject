bc6129eb2f86089999171bf523433ce50db82f2a
Archer-James <156135833+Archer-James@users.noreply.github.com>
2024-12-08 15:57:55 -0500

    changed colors and formatting for daily

--- AIPlanner/AIPlanner/classes/daily_cal.py
+++ AIPlanner/AIPlanner/classes/daily_cal.py
@@ -66,7 +66,7 @@ def daily() -> rx.Component:
                                 "color":
                                 Task.get_priority_color(task),
                                 "wordWrap": "break-word",  
-                                "maxWidth": "400px",
+                                "maxWidth": "800px",
                             },
                         ),
                         None,  # Otherwise, do not display anything
@@ -79,13 +79,15 @@ def daily() -> rx.Component:
                         # Filter tasks where the due date matches the selected date
                         task.assigned_block_date == daily_cal.selected_date,
                         rx.text(
-                            f"- {task.task_name}: {task.description}: {task.assigned_block_start_time}: {task.assigned_block_duration}",
+                            f"- {task.task_name}: {task.description}" 
+                            f"<br>Assigned start time: {task.assigned_block_start_time}"
+                            f"<br>Assigned block duration: {task.assigned_block_duration}",
                             style={
-                                "color":
-                                Task.get_priority_color(task),
+                                "color": Task.get_priority_color(task),
                                 "wordWrap": "break-word",
-                                "maxWidth": "400px",
+                                "maxWidth": "800px",
                             },
+                            dangerously_allow_html=True,  # Allows the use of HTML tags like <br>
                         ),
                         None,  # Otherwise, do not display anything
                     ),
